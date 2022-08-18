9d6a991ca31f736956ae0acc76b425f6900811ca
AP2015 <arian.press2015@gmail.com>
2022-08-18 18:13:20 +0430

    fix: rewrite FilterCategoryDto and UpdateCategoryDto

--- src/category/dto/filter-category.dto.ts
+++ src/category/dto/filter-category.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCategoryDto } from './create-category.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
-export class FilterCategoryDto extends PartialType(CreateCategoryDto) {}
+export class FilterCategoryDto {
+  @IsOptional()
+  @IsString({ message: 'Title must be a string' })
+  @ApiProperty({
+    example: 'beach',
+    description: 'title of the Category',
+  })
+  readonly title?: string;
+
+  @IsOptional()
+  @IsString({ message: 'Fa_title must be a string' })
+  @ApiProperty({
+    example: 'ساحلی و رو به دریا',
+    description: 'fa_title of the Category',
+  })
+  readonly fa_title?: string;
+}
