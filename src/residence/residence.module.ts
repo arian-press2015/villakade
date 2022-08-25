d4db826eeacb1e31c4f86e88c41e18d7c87f5e4c
AP2015 <arian.press2015@gmail.com>
2022-08-26 03:17:03 +0430

    feat: add HostJwtGuard to residence routes

--- src/residence/residence.module.ts
+++ src/residence/residence.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ResidenceService } from './residence.service';
 import { ResidenceController } from './residence.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ResidenceController],
   providers: [ResidenceService],
 })
