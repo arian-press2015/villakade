47f9d27ad310135ba431319cd6fa613a42cf7c27
AP2015 <arian.press2015@gmail.com>
2022-08-26 03:17:42 +0430

    feat: add HostJwtGuard to residenceImg routes

--- src/residenceImage/residenceImage.module.ts
+++ src/residenceImage/residenceImage.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ResidenceImageService } from './residenceImage.service';
 import { ResidenceImageController } from './residenceImage.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ResidenceImageController],
   providers: [ResidenceImageService],
 })
