e0eb527933b082b0181213c9f3757cc170fd5dd0
AP2015 <arian.press2015@gmail.com>
2022-09-05 22:27:13 +0430

    feat: add cities to province response

--- src/city/dto/city.dto.ts
+++ src/city/dto/city.dto.ts
@@ -25,3 +25,26 @@ export class City {
   })
   readonly total_residence_count: number;
 }
+
+export class ProvinceCity {
+  @ApiProperty({ example: 12345, description: 'ID of the entity in database' })
+  readonly id: number;
+
+  @ApiProperty({
+    example: 'shiraz',
+    description: 'name of the City',
+  })
+  readonly name: string;
+
+  @ApiProperty({
+    example: 'شیراز',
+    description: 'fa_name of the City',
+  })
+  readonly fa_name: string;
+
+  @ApiProperty({
+    example: 12,
+    description: 'residence count in this city',
+  })
+  readonly total_residence_count: number;
+}
--- src/province/dto/province.dto.ts
+++ src/province/dto/province.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ProvinceCity } from '../../city/dto';
 
 export class Province {
   @ApiProperty({ example: 12345, description: 'ID of the entity in database' })
@@ -15,4 +16,17 @@ export class Province {
     description: 'fa_name of the Province',
   })
   readonly fa_name: string;
+
+  @ApiProperty({
+    example: [
+      {
+        id: 123,
+        name: 'shiraz',
+        fa_name: 'شیراز',
+        total_residence_count: 12,
+      },
+    ],
+    description: 'cities of the Province',
+  })
+  readonly cities: ProvinceCity[];
 }
