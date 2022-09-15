126567e24b7e5895da6790def9af6362e269b6f3
AP2015 <arian.press2015@gmail.com>
2022-09-16 02:36:24 +0430

    fix: add missing dependency

--- src/type/type.module.ts
+++ src/type/type.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeService } from './type.service';
 import { TypeController } from './type.controller';
 import { AuthModule } from '../auth/auth.module';
+import { PrismaService } from '../shared/services/prisma.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [TypeController],
-  providers: [TypeService],
+  providers: [TypeService, PrismaService],
 })
 export class TypeModule {}
