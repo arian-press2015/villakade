e660019a4e85bc1a9b7fdec10d3b089a8f2e2dbb
AP2015 <arian.press2015@gmail.com>
2022-09-25 16:43:52 +0330

    feat(category): add image

--- src/category/dto/category.dto.ts
+++ src/category/dto/category.dto.ts
@@ -1,6 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { CategoryImage } from '../../categoryImage/dto';
 
 export class Category {
+  @ApiProperty({ example: 12345, description: 'ID of the entity in database' })
+  readonly id: number;
+
+  @ApiProperty({
+    example: 'beach',
+    description: 'title of the Category',
+  })
+  readonly title: string;
+
+  @ApiProperty({
+    example: 'ساحلی و رو به دریا',
+    description: 'fa_title of the Category',
+  })
+  readonly fa_title: string;
+
+  @ApiProperty({
+    example: {
+      category_id: 123,
+      url: '/here',
+      width: 400,
+      height: 500,
+    },
+    description: 'image of the Category',
+  })
+  readonly category_image?: CategoryImage;
+}
+
+export class RawCategory {
   @ApiProperty({ example: 12345, description: 'ID of the entity in database' })
   readonly id: number;
 
--- src/category/dto/index.ts
+++ src/category/dto/index.ts
@@ -1,4 +1,4 @@
 export { CreateCategoryDto } from './create-category.dto';
 export { UpdateCategoryDto } from './update-category.dto';
-export { Category } from './category.dto';
+export { Category, RawCategory } from './category.dto';
 export { FilterCategoryDto } from './filter-category.dto';
